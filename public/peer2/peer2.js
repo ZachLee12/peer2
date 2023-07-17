5c8a0ccd15a9febddb1f893957d579074aa67127
ZachLee12 <leezhengyang22@gmail.com>
2023-07-17 11:00:58 +0200

    Establish 2-way communication

--- public/peer2/peer2.js
+++ public/peer2/peer2.js
@@ -7,21 +7,37 @@ const peer2 = new Peer(peerId, {
     path: "/",
 })
 
+//Peer (sending messages)
+// 'open' = when connection to PeerServer is established
 peer2.on('open', (id) => console.log(`${id} connected to PeerServer`))
 peer2.on("error", (error) => {
     console.error(error)
 })
+const msgBtn = document.querySelector('.msg-btn')
+//Connect to a remote peer
 const conn = peer2.connect('Philipp')
-conn.on('data', function (data) {
-    console.log('Received:', data);
-});
+//DataConnection (receiving messages)
+conn.on('open', () => {
+    console.log(`%c[connection]: connection with ${conn.peer} established`, 'color:#35ce35;')
+})
+conn.on('close', () => {
+    console.log(`%c[connection]: connection with ${conn.peer} lost`, 'color:red;')
+})
 
+conn.on('data', data => console.log(`[message from ${conn.peer}]: ${data}`))
 
-// peer2.on('connection', (conn) => {
-//     conn.on('data', function (data) {
-//         console.log('Received:', data);
-//     });
-// })
+//When a remote peer is connected to you
+peer2.on('connection', dataConnection => {
+    console.log('[log]: ' + dataConnection.peer + " connected")
+    dataConnection.on('close', () => console.log(`%c[connection]: connection closed for ${dataConnection.peer}`, 'color:red;'))
+    dataConnection.on('data', data => console.log(`[message from ${dataConnection.peer}]: ${data}`))
+
+})
+
+msgBtn.addEventListener('click', () => {
+    conn.send('Hello Philipp, its me Zach')
+})
+
 
 peer2.on('call', (mediaConnection) => {
     console.log('call')
@@ -37,11 +53,4 @@ peer2.on('call', (mediaConnection) => {
         })
 })
 
-const msgBtn = document.querySelector('.msg-btn')
-msgBtn.addEventListener('click', () => {
-    conn.on('open', () => {
-        conn.send('Hello Philipp, its me Zach')
-        conn.on('error', (err) => console.error(err))
-    })
-})
 
